c810db46b358904889c0980efc3a91cb7a25170b
martenq1337 <martenq1337@gmail.com>
2020-09-23 14:50:58 +0200

    Added: implementation for sort controller

--- Assets/Scripts/Controllers/SortController.cs
+++ Assets/Scripts/Controllers/SortController.cs
@@ -1,6 +1,33 @@
-﻿namespace Assets.Scripts.Controllers
+﻿using Assets.Scripts.Shuffle;
+using Assets.Scripts.Sort;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers
 {
     class SortController
     {
+        public SortController()
+        {
+            this._bubbleSort = new BubbleSort();
+            this._linearSort = new LinearSort();
+        }
+
+        public void Sort(GameObject[] objects, Enums.Algorithm selectedSorting)
+        {
+            switch (selectedSorting)
+            {
+                case Enums.Algorithm.Linear:
+                    this._linearSort.Sort(objects);
+                    break;
+                case Enums.Algorithm.Bubble:
+                    this._bubbleSort.Sort(objects);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private BubbleSort _bubbleSort;
+        private LinearSort _linearSort;
     }
 }
