8756b68797a65a0a1dc9c2c79003e4f18c5e9d27
martenq1337 <martenq1337@gmail.com>
2020-09-23 12:14:47 +0200

    Modified: reorganize sorting algorithms

--- Assets/Scripts/Controller.cs
+++ Assets/Scripts/Controller.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using Assets.Scripts.Interfaces;
 
@@ -19,30 +18,12 @@ public class Controller : MonoBehaviour
         this._circleGenerator.ColorCalculator = this._colorCalculator;
         
         this._circles = this._circleGenerator.GenerateObjects();
-
-        this._randomSort = this.gameObject.AddComponent<RandomSort>();
-        this.Randomize();
-
-        this._sorter = this.gameObject.AddComponent<LinearSort>();
-        this.StartCoroutine(ExecuteAfterTime(3));
     }
 
     #region private
     private CircleGenerator _circleGenerator;
     private ColorCalculator _colorCalculator;
-    private ISort _sorter;
-    private ISort _randomSort;
 
     private GameObject[] _circles;
-    private void Randomize()
-    {
-        this._randomSort.Sort(this._circles);
-    }
-
-    IEnumerator ExecuteAfterTime(float time)
-    {
-        yield return new WaitForSeconds(time);
-        this._sorter.Sort(this._circles);
-    }
     #endregion
 }
---
+++ Assets/Scripts/Sort/RandomSort.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Assets.Scripts.Interfaces;
+
+namespace Assets.Scripts.Sort
+{
+    public class RandomSort : MonoBehaviour, ISort
+    {
+        public void Sort(GameObject[] circles)
+        {
+            int r0, r1;
+
+            int loop = 1000;
+            for (int i = 0; i < loop; i++)
+            {
+                r0 = Random.Range(0, circles.Length - 1);
+                r1 = Random.Range(0, circles.Length - 1);
+
+                Vector2 r0Position = circles[r0].GetComponent<Circle>().Position;
+                circles[r0].GetComponent<Circle>().Position = circles[r1].GetComponent<Circle>().Position;
+                circles[r1].GetComponent<Circle>().Position = r0Position;
+
+                int r0Place = circles[r0].GetComponent<Circle>().Place;
+                circles[r0].GetComponent<Circle>().Place = circles[r1].GetComponent<Circle>().Place;
+                circles[r1].GetComponent<Circle>().Place = r0Place;
+            }
+        }
+    }
+}
