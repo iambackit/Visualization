0c25d701a396a10044750d3a65162b3c62ca38f9
martenq1337 <martenq1337@gmail.com>
2020-09-24 14:45:51 +0200

    Fixed: selection sort

--- Assets/Scripts/Sort/SelectionSort.cs
+++ Assets/Scripts/Sort/SelectionSort.cs
@@ -14,41 +14,32 @@ namespace Assets.Scripts.Sort
             IsFinished = false;
             Dictionary<int, int> indicies = this.SetIndicies(objects);
 
-            //for (int i = 0; i < objects.Length - 1; i++)
-            //{
-            //    GameObject prev = objects[indicies[i]];
-            //    Color savedPrevColor = prev.GetComponent<Circle>().Color;
-            //    prev.GetComponent<Circle>().Color = Color.white;
+            for (int i = 0; i < objects.Length - 1; i++)
+            {
+                int smallestIdx = indicies[i];
 
-            //    for (int j = i + 1; j < objects.Length; j++)
-            //    {
-            //        GameObject next = objects[indicies[j]];
-            //        Color savedNextColor = next.GetComponent<Circle>().Color;
-            //        next.GetComponent<Circle>().Color = Color.white;
-
-            //        int prevPlace = prev.GetComponent<Circle>().Place;
-            //        int nextPlace = next.GetComponent<Circle>().Place;
-
-            //        if (indicies[i] > indicies[j])
-            //        {
-            //            Vector2 tmpPosition = prev.GetComponent<Circle>().Position;
-            //            prev.GetComponent<Circle>().Position = next.GetComponent<Circle>().Position;
-            //            next.GetComponent<Circle>().Position = tmpPosition;
-
-            //            int tmpPlace = prev.GetComponent<Circle>().Place;
-            //            prev.GetComponent<Circle>().Place = next.GetComponent<Circle>().Place;
-            //            next.GetComponent<Circle>().Place = tmpPlace;
-
-            //            int tmp = indicies[i];
-            //            indicies[i] = indicies[j];
-            //            indicies[j] = tmp;
-            //        }
+                for (int j = i + 1; j < objects.Length; j++)
+                {
+                    if (indicies[i] > indicies[j])
+                    {
+                        smallestIdx = j;
+                    }
 
                     yield return new WaitForSeconds(Time);
-            //        next.GetComponent<Circle>().Color = savedNextColor;
-            //    }
-            //    prev.GetComponent<Circle>().Color = savedPrevColor;
-            //}
+                }
+
+                Vector2 tmpPosition = objects[indicies[i]].GetComponent<Circle>().Position;
+                objects[indicies[i]].GetComponent<Circle>().Position = objects[indicies[smallestIdx]].GetComponent<Circle>().Position;
+                objects[indicies[smallestIdx]].GetComponent<Circle>().Position = tmpPosition;
+
+                int tmpPlace = objects[indicies[i]].GetComponent<Circle>().Value;
+                objects[indicies[i]].GetComponent<Circle>().Value = objects[indicies[smallestIdx]].GetComponent<Circle>().Value;
+                objects[indicies[smallestIdx]].GetComponent<Circle>().Value = tmpPlace;
+
+                int tmp = indicies[i];
+                indicies[i] = indicies[smallestIdx];
+                indicies[smallestIdx] = tmp;
+            }
 
             IsFinished = true;
         }
